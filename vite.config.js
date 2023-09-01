4c1442289943fc5b1362c93fba5281f277f8ef25
SparkkyJD <wagnd4e@gmail.com>
2023-09-01 18:51:09 -0400

    f

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: path.resolve(__dirname, 'dist'),
   },
   server: {
     port: 3001,
-    open: true
-  }
-})
+    open: true,
+  },
+});
