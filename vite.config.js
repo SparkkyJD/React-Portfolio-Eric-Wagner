0c1172e71c3064d8ba5862d850ddd2369a7ea01c
SparkkyJD <wagnd4e@gmail.com>
2023-09-01 15:43:43 -0400

    fixed heroku delpoy error

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    minify: 'terser', 
+    base: 'https://eric-wagner-portfolio.herokuapp.com', 
+  },
   server: {
-    port: 3001,
-    open: true
+    port: process.env.PORT || 3001,
   }
 })
