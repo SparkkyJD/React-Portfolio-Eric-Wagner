26500f783f74c4dfbad85136a0ad108183669886
SparkkyJD <wagnd4e@gmail.com>
2023-09-01 16:27:35 -0400

    removed terser

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    minify: 'terser'
+    outDir: 'dist'
   },
   server: {
     port: 3001,
