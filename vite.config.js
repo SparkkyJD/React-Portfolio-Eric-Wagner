78f34fba137e4a1a4bcae5e34e7b26d92716a150
SparkkyJD <wagnd4e@gmail.com>
2023-09-01 17:18:06 -0400

    fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,18 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      input: '/src/main.jsx'
+    }
   },
   server: {
     port: 3001,
     open: true
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
   }
 })
