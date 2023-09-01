9b9d0b0c3e3e2cc065a7f845862582bd76881bb3
SparkkyJD <wagnd4e@gmail.com>
2023-09-01 16:18:10 -0400

    added viteconfig path

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     minify: 'terser',
-    base: 'React-Portfolio-Eric-Wagner', 
+    base: '/React-Portfolio-Eric-Wagner', 
   },
   server: {
     port: 3001,
